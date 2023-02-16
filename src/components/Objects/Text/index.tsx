4f96de27cc5db592e1e3262dd5db085c3da6b723
Theo Gregory <tjmg@outlook.com>
2023-02-16 10:12:39 +0700

    feat: gets mode switching working

--- src/components/Objects/Text/index.tsx
+++ src/components/Objects/Text/index.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import EditorMode from "../../../commmon/EditorMode";
+import { useEditorContext } from "../../Editor/EditorContext";
 
-const Text: React.FC<{ mode: EditorMode }> = ({ mode }) => {
+const Text: React.FC = () => {
+  const { editorMode } = useEditorContext();
+  return <TextOrInput editorMode={editorMode} />;
+};
+
+const TextOrInput: React.FC<{ editorMode: EditorMode }> = ({ editorMode }) => {
   const [value, setValue] = useState("");
-  if (mode === EditorMode.Edit) {
-    return <input onChange={(e) => setValue(e.target.value)} />;
+  if (editorMode === EditorMode.Edit) {
+    return <input value={value} onChange={(e) => setValue(e.target.value)} />;
   }
-  return <h1>value</h1>;
+  return <h1>{value}</h1>;
 };
 
 export default Text;
