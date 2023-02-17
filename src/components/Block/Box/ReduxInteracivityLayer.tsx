45ac1455cb8f0b305f8e4d89805b9a0fb959a3db
Theo Gregory <tjmg@outlook.com>
2023-02-17 16:33:02 +0700

    feat: removes interactivity layers when editor mode is preview

--- src/components/Block/Box/ReduxInteracivityLayer.tsx
+++ src/components/Block/Box/ReduxInteracivityLayer.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from "react";
 import Block from "../../../commmon/Block";
+import EditorMode from "../../../commmon/EditorMode";
 import { useAppDispatch } from "../../../commmon/redux/hooks";
 import { addChildBlock } from "../../../features/blockEditorSlice";
+import { useEditorContext } from "../../Editor/EditorContext";
 
 const ReduxBlockController: React.FC<{
   statePath: string;
-  children: ({ addChild }: { addChild?: () => void }) => ReactElement;
+  children: (props: { addChild?: () => void }) => ReactElement;
 }> = ({ statePath, children }) => {
   const dispatch = useAppDispatch();
   const addChild = () =>
@@ -27,12 +29,18 @@ export const InteractivityLayer: React.FC<
 
 const ReduxInteractivityLayer: React.FC<
   React.PropsWithChildren<{ statePath: string }>
-> = ({ statePath, children }) => (
-  <ReduxBlockController statePath={statePath}>
-    {({ addChild }) => (
-      <InteractivityLayer addChild={addChild}>{children}</InteractivityLayer>
-    )}
-  </ReduxBlockController>
-);
+> = ({ statePath, children }) => {
+  const { editorMode } = useEditorContext();
+  if (editorMode === EditorMode.Preview) {
+    return <>{children}</>;
+  }
+  return (
+    <ReduxBlockController statePath={statePath}>
+      {({ addChild }) => (
+        <InteractivityLayer addChild={addChild}>{children}</InteractivityLayer>
+      )}
+    </ReduxBlockController>
+  );
+};
 
 export default ReduxInteractivityLayer;
