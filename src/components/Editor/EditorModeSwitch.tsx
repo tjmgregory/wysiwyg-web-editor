1d72737cfae4e1a956d608a5a85bd20599b9cba6
Theo Gregory <tjmg@outlook.com>
2023-02-17 17:56:56 +0700

    feat: adds a toggle button

--- src/components/Editor/EditorModeSwitch.tsx
+++ src/components/Editor/EditorModeSwitch.tsx
@@ -1,24 +1,46 @@
 import EditorMode from "../../commmon/EditorMode";
 import { useEditorContext } from "./EditorContext";
+import styled from "styled-components";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 
 const EditorModeSwitch: React.FC = () => {
   const { editorMode, setEditorMode } = useEditorContext();
   return (
-    <>
-      <p>{editorMode}</p>
-      <button
-        onClick={() =>
-          setEditorMode(
-            editorMode === EditorMode.Edit
-              ? EditorMode.Preview
-              : EditorMode.Edit
-          )
-        }
-      >
-        {"Change mode"}
-      </button>
-    </>
+    <Root
+      type="single"
+      defaultValue={editorMode}
+      aria-label="Editor mode"
+      onValueChange={setEditorMode}
+    >
+      <Item value={EditorMode.Edit}>{"Edit"}</Item>
+      <Item value={EditorMode.Preview}>{"Preview"}</Item>
+    </Root>
   );
 };
+
+const Root = styled(ToggleGroup.Root)`
+  --background-color: lightgrey;
+  display: inline-flex;
+  box-shadow: 0 2px 10px #666666;
+  border-radius: 4px;
+  overflow: hidden;
+  background-color: var(--background-color);
+`;
+
+const Item = styled(ToggleGroup.Item)`
+  background-color: var(--background-color);
+  outline: none;
+  border: none;
+  padding: 8px 16px;
+  transition: background-color 0.1s;
+
+  &:hover {
+    background-color: white;
+  }
+
+  &[data-state="on"] {
+    background-color: white;
+  }
+`;
 
 export default EditorModeSwitch;
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { ReduxBlockSelector } from "../Block/Box/ReduxBlockSelector";
 import { EditorContextProvider } from "./EditorContext";
 import EditorModeSwitch from "./EditorModeSwitch";
@@ -5,10 +6,18 @@ import EditorModeSwitch from "./EditorModeSwitch";
 const Editor: React.FC = () => {
   return (
     <EditorContextProvider>
-      <EditorModeSwitch />
       <ReduxBlockSelector key="root" statePath="root" />
+      <Toolbar>
+        <EditorModeSwitch />
+      </Toolbar>
     </EditorContextProvider>
   );
 };
+
+const Toolbar = styled.div`
+  position: absolute;
+  bottom: 0;
+  margin: auto;
+`;
 
 export default Editor;
